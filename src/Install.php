af8d9e59ba697627b8b0e22d5d3b952e4f9d1dc0
hsioe <dawei0937@gmail.com>
2024-06-12 16:37:08 +0800

    fix: 修复安装自动加载时无法自动加载命令行

--- src/Install.php
+++ src/Install.php
@@ -10,7 +10,7 @@ class Install
      * @var array
      */
     protected static $pathRelation = [
-        'config/plugin/hsioe-job' => 'config/plugin/hsioe-job'
+        'config/plugin/hsioe/jobs' => 'config/plugin/hsioe/jobs'
     ];
     
     /**
